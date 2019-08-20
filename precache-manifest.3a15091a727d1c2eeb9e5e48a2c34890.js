29a2d73ff035b054fb180b1c04539f4320fd9747
farajsharman <farajsharman@yahoo.com>
2019-08-20 10:32:02 +0100

    Updates

---
+++ precache-manifest.3a15091a727d1c2eeb9e5e48a2c34890.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "adfd94ce77dc463fe208853fa0ae12f1",
+    "url": "/farajsharman/portfolio.git/index.html"
+  },
+  {
+    "revision": "6af68b38849ce71117c3",
+    "url": "/farajsharman/portfolio.git/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "b5655ee240e08b75df5b",
+    "url": "/farajsharman/portfolio.git/static/js/2.0f2e91db.chunk.js"
+  },
+  {
+    "revision": "6af68b38849ce71117c3",
+    "url": "/farajsharman/portfolio.git/static/js/main.c2cd3991.chunk.js"
+  },
+  {
+    "revision": "4172936245f991981c9c",
+    "url": "/farajsharman/portfolio.git/static/js/runtime~main.667ad54d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/farajsharman/portfolio.git/static/media/logo.5d5d9eef.svg"
+  }
+]);
